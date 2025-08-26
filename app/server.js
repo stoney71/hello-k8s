120a98730c58e2cd6a4ff3aa4cf4438ae1a9fdec
Michael Stone <mjstone01@yahoo.com>
2025-08-26 17:56:13 +0930

    Update server.js

--- app/server.js
+++ app/server.js
@@ -2,6 +2,6 @@ const http = require('http');
 const port = process.env.PORT || 8080;
 const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type':'text/plain'});
-  res.end(`hello-k8s :-)  path=${req.url}\n`);
+  res.end(`hello-k8s :->  path=${req.url}\n`);
 });
 server.listen(port, () => console.log(`listening on ${port}`));
